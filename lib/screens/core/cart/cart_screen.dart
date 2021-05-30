28361fc5297af6df0ff00a5f1f08f46e54cdeac6
Lino <u201819681@upc.edu.pe>
2021-05-30 00:11:14 -0500

    cart validation

--- lib/screens/core/cart/cart_screen.dart
+++ lib/screens/core/cart/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:foodyeah/common/Messages.dart';
 import 'package:foodyeah/models/Order.dart';
 import 'package:foodyeah/models/OrderDetail.dart';
 import 'package:foodyeah/models/Product.dart';
@@ -9,6 +10,8 @@ import 'package:foodyeah/providers/customer_provider.dart';
 import 'package:foodyeah/providers/orders_provider.dart';
 import 'package:foodyeah/providers/products_provider.dart';
 import 'package:foodyeah/screens/core/cart/cart_item.dart';
+import 'package:foodyeah/services/notification_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class CartScreen extends StatefulWidget {
@@ -39,34 +42,42 @@ class _CartScreenState extends State<CartScreen> {
         loading = true;
       });
       var cart = cartProvider.items;
-      var customerdata = await customerProvider.getDataFromJwt();
-      var customer =
-          await customerProvider.getCustomerByEmail(customerdata['email']);
-      QuoteDetail quoteDetails =
-          new QuoteDetail(int.parse(customer.customerId!));
+      if (cartProvider.itemCount > 0) {
+        var customerdata = await customerProvider.getDataFromJwt();
+        var customer =
+            await customerProvider.getCustomerByEmail(customerdata['email']);
+        QuoteDetail quoteDetails =
+            new QuoteDetail(int.parse(customer.customerId!));
 
-      List<OrderDetailCreateDto> details = [];
-      cart.forEach((key, value) {
-        OrderDetailCreateDto orderCreated = OrderDetailCreateDto(
-            productId: int.parse(value.productId!), quantity: value.quantity);
-        details.add(orderCreated);
-      });
+        List<OrderDetailCreateDto> details = [];
+        cart.forEach((key, value) {
+          OrderDetailCreateDto orderCreated = OrderDetailCreateDto(
+              productId: int.parse(value.productId!), quantity: value.quantity);
+          details.add(orderCreated);
+        });
 
-      CreateOrderDto createOrderDto = CreateOrderDto(
-          int.parse(customer.customerId!), details, quoteDetails);
-      orderProvider.createOrder(createOrderDto, context).then((value) {
-        if (value) {
-          cartProvider.clear();
-          setState(() {
-            loading = false;
-          });
-        } else {
-          setState(() {
-            loading = false;
-          });
-          return;
-        }
-      });
+        CreateOrderDto createOrderDto = CreateOrderDto(
+            int.parse(customer.customerId!), details, quoteDetails);
+        orderProvider.createOrder(createOrderDto, context).then((value) {
+          if (value) {
+            cartProvider.clear();
+            setState(() {
+              loading = false;
+            });
+          } else {
+            setState(() {
+              loading = false;
+            });
+            return;
+          }
+        });
+      } else {
+        setState(() {
+          loading = false;
+        });
+        NotificationService().showSnackbar(
+            context, Messages().errorNoItemsInCart, "error", null);
+      }
     }
 
     return Scaffold(
@@ -137,54 +148,73 @@ class _CartScreenState extends State<CartScreen> {
             Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height * 0.65,
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemBuilder: (ctx, index) {
-                  return FutureBuilder(
-                    future: productProvider.getProductById(
-                        cartProvider.items.values.toList()[index].productId!),
-                    builder: (ctx, snapshot) {
-                      if (snapshot.hasData) {
-                        if (snapshot.data != null) {
-                          var product = snapshot.data as Product;
+              child: cartProvider.itemCount > 0
+                  ? ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemBuilder: (ctx, index) {
+                        return FutureBuilder(
+                          future: productProvider.getProductById(cartProvider
+                              .items.values
+                              .toList()[index]
+                              .productId!),
+                          builder: (ctx, snapshot) {
+                            if (snapshot.hasData) {
+                              if (snapshot.data != null) {
+                                var product = snapshot.data as Product;
 
-                          return CartItems(
-                              cartProvider.items.values
-                                  .toList()[index]
-                                  .productId!,
-                              cartProvider.items.values.toList()[index].price!,
-                              cartProvider.items.values
-                                  .toList()[index]
-                                  .quantity!,
-                              product.name.toString(),
-                              product.imageUrl.toString());
-                        } else {
-                          return Container(
-                            width: double.infinity,
-                            height: MediaQuery.of(context).size.height * 0.8,
-                            child: Text("Load"),
-                          );
-                        }
-                      } else {
-                        return Center(
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            margin: EdgeInsets.only(top: 40),
-                            child: CircularProgressIndicator(
-                              backgroundColor: Colors.white,
-                              valueColor: new AlwaysStoppedAnimation<Color>(
-                                  Colors.blue),
-                            ),
-                          ),
+                                return CartItems(
+                                    cartProvider.items.values
+                                        .toList()[index]
+                                        .productId!,
+                                    cartProvider.items.values
+                                        .toList()[index]
+                                        .price!,
+                                    cartProvider.items.values
+                                        .toList()[index]
+                                        .quantity!,
+                                    product.name.toString(),
+                                    product.imageUrl.toString());
+                              } else {
+                                return Container(
+                                  width: double.infinity,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.8,
+                                  child: Text("Load"),
+                                );
+                              }
+                            } else {
+                              return Center(
+                                child: Container(
+                                  width: 50,
+                                  height: 50,
+                                  margin: EdgeInsets.only(top: 40),
+                                  child: CircularProgressIndicator(
+                                    backgroundColor: Colors.white,
+                                    valueColor:
+                                        new AlwaysStoppedAnimation<Color>(
+                                            Colors.blue),
+                                  ),
+                                ),
+                              );
+                            }
+                          },
                         );
-                      }
-                    },
-                  );
-                },
-                itemCount: cartProvider.itemCount,
-              ),
+                      },
+                      itemCount: cartProvider.itemCount,
+                    )
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                          Icon(
+                            Icons.remove_shopping_cart,
+                            size: 50,
+                          ),
+                          Text(
+                              "No ha agregado ningun producto al carrito de compras",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.varelaRound(fontSize: 25))
+                        ]),
             ),
           ],
         ),
