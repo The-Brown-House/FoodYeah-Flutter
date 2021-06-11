4aa3f73ad59066ea9729f422ce30edd060fb97ce
Lino <u201819681@upc.edu.pe>
2021-06-11 17:09:54 -0500

    Update customer_screen.dart

--- lib/screens/core/customer/customer_screen.dart
+++ lib/screens/core/customer/customer_screen.dart
@@ -7,14 +7,13 @@ import 'package:provider/provider.dart';
 
 class CustomerScreen extends StatefulWidget {
   const CustomerScreen();
-  static const routeName = "/customers";
+  static const routeName = "/customer";
 
   @override
   _CustomerScreenState createState() => _CustomerScreenState();
 }
 
 class _CustomerScreenState extends State<CustomerScreen> {
-
   @override
   Widget build(BuildContext context) {
     var customerProvider = Provider.of<Customers>(context);
@@ -23,6 +22,7 @@ class _CustomerScreenState extends State<CustomerScreen> {
       var items = await customerProvider.getCustomerByEmail(tokenData['email']);
       return items;
     }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Mi perfil'),
@@ -38,74 +38,71 @@ class _CustomerScreenState extends State<CustomerScreen> {
                 tag: 'AnimationSpeed',
                 child: CircleAvatar(
                   radius: 100.0,
-                  backgroundImage: NetworkImage('https://media.discordapp.net/attachments/708078392376950807/839709195166941184/Picture3.jpg'),
+                  backgroundImage: NetworkImage(
+                      'https://media.discordapp.net/attachments/708078392376950807/839709195166941184/Picture3.jpg'),
                 ),
               ),
             ),
             FutureBuilder(
-              future: getCustomerData(),
-              builder: (ctx, snapshot){
-                if(snapshot.hasData){
-                  if(snapshot.data!=null){
-                    var items = snapshot.data as Customer;
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        FadeAnimation(
-                          Card(
-                            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
-                            child: ListTile(
-                              leading: Icon(Icons.account_circle, color: Colors.redAccent),
-                              title: Text(
-                                items.firstname! + ' ' + items.lastname!,
-                                style: GoogleFonts.varelaRound(
-                                    color: Colors.black, fontSize: 18)
+                future: getCustomerData(),
+                builder: (ctx, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data != null) {
+                      var items = snapshot.data as Customer;
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          FadeAnimation(
+                              Card(
+                                margin: EdgeInsets.symmetric(
+                                    vertical: 5.0, horizontal: 20.0),
+                                child: ListTile(
+                                  leading: Icon(Icons.account_circle,
+                                      color: Colors.redAccent),
+                                  title: Text(
+                                      items.firstname! + ' ' + items.lastname!,
+                                      style: GoogleFonts.varelaRound(
+                                          color: Colors.black, fontSize: 18)),
+                                ),
                               ),
-                            ),
-                          ),
-                            450,
-                            1
-                        ),
-                        FadeAnimation(
-                          Card(
-                            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
-                            child: ListTile(
-                              leading: Icon(Icons.alternate_email, color: Colors.redAccent),
-                              title: Text(
-                                items.email!,
-                                style: GoogleFonts.varelaRound(
-                                    color: Colors.black, fontSize: 18)
+                              450,
+                              1),
+                          FadeAnimation(
+                              Card(
+                                margin: EdgeInsets.symmetric(
+                                    vertical: 5.0, horizontal: 20.0),
+                                child: ListTile(
+                                  leading: Icon(Icons.alternate_email,
+                                      color: Colors.redAccent),
+                                  title: Text(items.email!,
+                                      style: GoogleFonts.varelaRound(
+                                          color: Colors.black, fontSize: 18)),
+                                ),
                               ),
-                            ),
-                          ),
-                            500,
-                            1
+                              500,
+                              1),
+                        ],
+                      );
+                    } else
+                      return Container(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.8,
+                        child: Text("Load"),
+                      );
+                  } else
+                    return Center(
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        margin: EdgeInsets.only(top: 40),
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.white,
+                          valueColor:
+                              new AlwaysStoppedAnimation<Color>(Colors.blue),
                         ),
-                      ],
-                    );
-                  }
-                  else
-                    return Container(
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height * 0.8,
-                      child: Text("Load"),
-                    );
-                }
-                else
-                  return Center(
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      margin: EdgeInsets.only(top: 40),
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.white,
-                        valueColor:
-                        new AlwaysStoppedAnimation<Color>(Colors.blue),
                       ),
-                    ),
-                  );
-              }
-            ),
+                    );
+                }),
           ],
         ),
       ),
